7e0fff48eb76a424462a6101f0f8892eff358c5d
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-05-09 21:53:55 +0530

    Added new files

---
+++ chapter_01/basics.js
@@ -0,0 +1,11 @@
+console.log("ravikishan Gaur is on the way")
+
+
+const studentId = 1234
+let studentName = "ravi"
+var studentAge = 18
+studentGender = "Male"
+
+let studentSalary = true
+
+console.table([studentId,studentName,studentAge,studentGender,studentSalary])
