daaab6a85381a7345348de2970c715062951dd4e
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-05-11 20:58:46 +0530

    Added more files

---
+++ chapter_02/conversionoperation.js
@@ -0,0 +1,19 @@
+let score = true;
+
+// console.log(typeof score);
+
+// let value = Number(score);
+// console.log(typeof value);
+// console.log(value);
+
+// "33" => 33
+// "33ad" ==> NaN
+// true ==> 1
+// faslse ==> 0
+
+console.log(1 + 2);
+console.log(typeof (1 + 2));      
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "1");
+console.log("1" + 2 + 3);
